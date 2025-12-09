3659e8f47a882ee28dbf93f54a0d408524816d90
Meysam <meysamsayyadtala@gmail.com>
2025-12-09 20:29:49 +0330

    EX_2 started

---
+++ EX_2/main.py
@@ -0,0 +1,50 @@
+from mlxtend.frequent_patterns import apriori,fpgrowth,association_rules
+from mlxtend.preprocessing import TransactionEncoder
+import pandas as pd
+import time 
+
+data= [['A', 'B','C' ,'E'],
+['B', 'D'],
+['B', 'C'],
+['A' ,'B' ,'D'],
+['A', 'B', 'C', 'E'],
+['A', 'B', 'C', 'E'],
+['A' ,'B', 'C', 'E'],
+['A' ,'B', 'D'],
+['B', 'C', 'E'],
+]
+
+te =TransactionEncoder()
+te_ary=te.fit(data).transform(data)
+
+items = sorted(set(item for t in data for item in t))
+df = pd.DataFrame(te_ary,columns=te.columns_)
+start = time.time()
+frequent_patterns= apriori(df,min_support=0.12,use_colnames=True)
+end=time.time()
+start_FPGrowth=time.time()
+frequent_patterns_FPGrowth= fpgrowth(df,min_support=0.12,use_colnames=True)
+
+end_FPGrowth=time.time()
+print('min_sup = 1\n')
+
+print(frequent_patterns.T ,f'\ntime used for Apriori :{end-start}')
+print(frequent_patterns_FPGrowth.T ,f'\ntime used FP growth :{end_FPGrowth-start_FPGrowth}')
+print('Apriori - FPGrowth',(end-start)-(end_FPGrowth-start_FPGrowth))
+
+items = sorted(set(item for t in data for item in t))
+df = pd.DataFrame(te_ary,columns=te.columns_)
+start = time.time()
+frequent_patterns= apriori(df,min_support=0.67,use_colnames=True)
+end=time.time()
+start_FPGrowth=time.time()
+frequent_patterns_FPGrowth= fpgrowth(df,min_support=0.67,use_colnames=True)
+end_FPGrowth=time.time()
+print('\nmin_sup = 6\n')
+print(frequent_patterns.T ,f'\ntime used for Apriori :{end-start}')
+print(frequent_patterns_FPGrowth.T ,f'\ntime used FP growth :{end_FPGrowth-start_FPGrowth}')
+print('Apriori - FPGrowth',(end-start)-(end_FPGrowth-start_FPGrowth))
+
+
+
+
