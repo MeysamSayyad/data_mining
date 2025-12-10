92de5ebf0d68d09aa6543feb1494abd2961726d2
Meysam <meysamsayyadtala@gmail.com>
2025-12-10 18:09:41 +0330

    EX_2 theory almost finished

--- EX_2/theory.py
+++ EX_2/theory.py
@@ -17,7 +17,14 @@ data= [['A', 'B','C' ,'E'],
 te =TransactionEncoder()
 te_ary=te.fit(data).transform(data)
 
-items = sorted(set(item for t in data for item in t))
+
+df = pd.DataFrame(te_ary,columns=te.columns_)
+
+
+frequent_patterns_FPGrowth= fpgrowth(df,min_support=0.33,use_colnames=True)
+accociationRules= association_rules(frequent_patterns_FPGrowth)
+print(accociationRules)
+
 df = pd.DataFrame(te_ary,columns=te.columns_)
 start = time.time()
 frequent_patterns= apriori(df,min_support=0.12,use_colnames=True)
@@ -32,7 +39,6 @@ print(frequent_patterns.T ,f'\ntime used for Apriori :{end-start}')
 print(frequent_patterns_FPGrowth.T ,f'\ntime used FP growth :{end_FPGrowth-start_FPGrowth}')
 print('Apriori - FPGrowth',(end-start)-(end_FPGrowth-start_FPGrowth))
 
-items = sorted(set(item for t in data for item in t))
 df = pd.DataFrame(te_ary,columns=te.columns_)
 start = time.time()
 frequent_patterns= apriori(df,min_support=0.67,use_colnames=True)
@@ -45,6 +51,15 @@ print(frequent_patterns.T ,f'\ntime used for Apriori :{end-start}')
 print(frequent_patterns_FPGrowth.T ,f'\ntime used FP growth :{end_FPGrowth-start_FPGrowth}')
 print('Apriori - FPGrowth',(end-start)-(end_FPGrowth-start_FPGrowth))
 
-
-
-
+df_Grades_Org=pd.read_csv('EX_2/Grades_Org.csv')
+print(df_Grades_Org.mean())
+print(df_Grades_Org.std())
+normalized_df=(df_Grades_Org-df_Grades_Org.mean())/df_Grades_Org.std()
+print(normalized_df)
+df_Grades=pd.read_csv('EX_2/Grades.csv')
+print(df_Grades)
+print(df_Grades['Final score (out of 20)(y)'].sum())
+print((df_Grades['Study hours per week(x1)-Norm ']*-df_Grades['Final score (out of 20)(y)']))
+print((df_Grades['Study hours per week(x1)-Norm ']*-df_Grades['Final score (out of 20)(y)']).sum())
+print((df_Grades['Bedtime(x2)-Norm']*-df_Grades['Final score (out of 20)(y)']))
+print((df_Grades['Bedtime(x2)-Norm']*-df_Grades['Final score (out of 20)(y)']).sum())
