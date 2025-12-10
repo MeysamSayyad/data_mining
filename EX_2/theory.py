43de63898b08a90d078a529301a2b585d29fcc25
Meysam <meysamsayyadtala@gmail.com>
2025-12-10 21:23:48 +0330

    minor additions

--- EX_2/theory.py
+++ EX_2/theory.py
@@ -3,7 +3,7 @@ from mlxtend.preprocessing import TransactionEncoder
 import pandas as pd
 import time 
 
-data= [['A', 'B','C' ,'E'],
+FrequentData= [['A', 'B','C' ,'E'],
 ['B', 'D'],
 ['B', 'C'],
 ['A' ,'B' ,'D'],
@@ -13,9 +13,9 @@ data= [['A', 'B','C' ,'E'],
 ['A' ,'B', 'D'],
 ['B', 'C', 'E'],
 ]
-
+# Transforming FrequentData  for using as input for Fp-growth and Apriori
 te =TransactionEncoder()
-te_ary=te.fit(data).transform(data)
+te_ary=te.fit(FrequentData).transform(FrequentData)
 
 
 df = pd.DataFrame(te_ary,columns=te.columns_)
@@ -23,8 +23,10 @@ df = pd.DataFrame(te_ary,columns=te.columns_)
 
 frequent_patterns_FPGrowth= fpgrowth(df,min_support=0.33,use_colnames=True)
 accociationRules= association_rules(frequent_patterns_FPGrowth)
+# Getting Valid accociation Rules
 print(accociationRules)
 
+# Measuring Calculation Time with Support 1 for Apriori & FP-growth
 df = pd.DataFrame(te_ary,columns=te.columns_)
 start = time.time()
 frequent_patterns= apriori(df,min_support=0.12,use_colnames=True)
@@ -39,6 +41,8 @@ print(frequent_patterns.T ,f'\ntime used for Apriori :{end-start}')
 print(frequent_patterns_FPGrowth.T ,f'\ntime used FP growth :{end_FPGrowth-start_FPGrowth}')
 print('Apriori - FPGrowth',(end-start)-(end_FPGrowth-start_FPGrowth))
 
+
+# Measuring Calculation Time with Support 6 for Apriori & FP-growth
 df = pd.DataFrame(te_ary,columns=te.columns_)
 start = time.time()
 frequent_patterns= apriori(df,min_support=0.67,use_colnames=True)
@@ -50,6 +54,7 @@ print('\nmin_sup = 6\n')
 print(frequent_patterns.T ,f'\ntime used for Apriori :{end-start}')
 print(frequent_patterns_FPGrowth.T ,f'\ntime used FP growth :{end_FPGrowth-start_FPGrowth}')
 print('Apriori - FPGrowth',(end-start)-(end_FPGrowth-start_FPGrowth))
+
 
 df_Grades_Org=pd.read_csv('EX_2/Grades_Org.csv')
 print(df_Grades_Org.mean())
@@ -63,3 +68,11 @@ print((df_Grades['Study hours per week(x1)-Norm ']*-df_Grades['Final score (out 
 print((df_Grades['Study hours per week(x1)-Norm ']*-df_Grades['Final score (out of 20)(y)']).sum())
 print((df_Grades['Bedtime(x2)-Norm']*-df_Grades['Final score (out of 20)(y)']))
 print((df_Grades['Bedtime(x2)-Norm']*-df_Grades['Final score (out of 20)(y)']).sum())
+secondTry=1.465+ (df_Grades['Study hours per week(x1)-Norm ']*0.37693)+(df_Grades['Bedtime(x2)-Norm']*-0.32034)
+secondtry_diffrence=secondTry - df_Grades['Final score (out of 20)(y)']
+secondTry_multiply_x1=df_Grades['Study hours per week(x1)-Norm ']*secondtry_diffrence
+secondTry_multiply_x2=df_Grades['Bedtime(x2)-Norm']*secondtry_diffrence
+secondTry_multiply_1=(secondtry_diffrence.sum()*0.1)/20
+print((secondTry_multiply_x1.sum()*0.1)/20)
+print((secondTry_multiply_x2.sum()*0.1)/20)
+print(secondTry_multiply_1)
