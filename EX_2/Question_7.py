53e36c53c363e61b2af6c0f38697eb8be5a0bf1d
Meysam <meysamsayyadtala@gmail.com>
2025-12-13 21:52:36 +0330

    EX_2 fully completed

--- EX_2/Question_7.py
+++ EX_2/Question_7.py
@@ -6,8 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler,LabelEncoder
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score,classification_report,log_loss
+from sklearn.metrics import accuracy_score,classification_report,log_loss,confusion_matrix
 from sklearn.decomposition import PCA
+from sklearn.inspection import DecisionBoundaryDisplay
 import matplotlib.pyplot as plt
 
 df_clean=df_clean.drop(['Invoice ID','Date','Time',],axis=1)
@@ -19,16 +20,24 @@ val,test=train_test_split(temp,test_size=0.5,shuffle=True,random_state=42)
 
 
 class_field='Customer type'
+
 X_train = train.drop(class_field,axis=1)
 X_train=pd.get_dummies(X_train)
+
 Y_train=train[class_field]
+
 X_val = val.drop(class_field,axis=1)
 X_val=pd.get_dummies(X_val)
+
 Y_val=val[class_field]
+
 X_test = test.drop(class_field,axis=1)
 X_test=pd.get_dummies(X_test)
+
 Y_test=test[class_field]
+
 model_pca = LogisticRegression(max_iter=2000)
+
 scaler=StandardScaler()
 X_train=scaler.fit_transform(X_train)
 X_val=scaler.fit_transform(X_val)
@@ -39,28 +48,53 @@ X_val_pca=pca.fit_transform(X_val)
 X_test_pca=pca.fit_transform(X_test)
 model_pca.fit(X_train_pca,Y_train)
 
-param_grid={'C':[0.01,0.1,10,100],'solver':['lbfgs'],'max_iter':[2000]}
+param_grid={'C':[0.001,0.01,0.1,10,100],'solver':['lbfgs'],'max_iter':[2000]}
 le=LabelEncoder()
 
 Y_encoded=le.fit_transform(Y_train)
-x_min, x_max = X_train_pca[:,0].min() - 1, X_train_pca[:,0].max() + 1
-y_min, y_max = X_train_pca[:,1].min() - 1, X_train_pca[:,1].max() + 1
-xx, yy = np.meshgrid(np.linspace(x_min, x_max, 200),
-                     np.linspace(y_min, y_max, 200))
+Y_encoded_test=le.fit_transform(Y_test)
+grid =GridSearchCV(estimator=model_pca,param_grid=param_grid,cv=5,scoring='accuracy',n_jobs=1)
+grid.fit(X_val,Y_val)
+print('Best C:',grid.best_params_['C'])
+print('Best CV Accuracy:',grid.best_score_)
+# best_model=grid.best_estimator_
+# best_model.fit(X_train,Y_train)
+print('-'*40)
 
 
-# Predict on the grid
-Z = model_pca.predict(np.c_[xx.ravel(), yy.ravel()])
-if Z.dtype.kind in {'U','O'}:
-    Z = le.transform(Z)  # or convert to numeric codes
+best_model =LogisticRegression(C=100000000,max_iter=2000)
+best_model.fit(X_train_pca,Y_train)
+X_final_pca=np.vstack([X_train_pca,X_val_pca])
+Y_final=np.hstack([Y_train,Y_val])
 
-Z = Z.reshape(xx.shape)
+best_model.fit(X_final_pca,Y_final)
+y_pred=best_model.predict(X_train_pca)
+y_pred_test=best_model.predict(X_test_pca)
 
-# Plot
-plt.figure(figsize=(8,6))
-plt.contourf(xx, yy, Z, alpha=0.3, cmap='coolwarm')
-plt.scatter(X_train_pca[:,0], X_train_pca[:,1], c=Y_encoded, edgecolor='k', cmap='coolwarm')
-plt.xlabel('X train')
-plt.ylabel('Y train')
-plt.title('Decision Boundary in PCA 2D space')
+
+
+display=DecisionBoundaryDisplay.from_estimator(best_model,X_train_pca,response_method='predict',response=y_pred,cmap=plt.cm.RdYlBu,alpha=0.3)
+display.plot()
+display.ax_.set_xlabel('PC1')
+display.ax_.set_ylabel('PC2')
+display.ax_.set_title('Decision Boundary Scatter Traindata')
+display.ax_.scatter(
+    X_train_pca[:, 0],X_train_pca[:, 1], c=Y_encoded, edgecolor="black",label='Member'
+)
+display.ax_.legend()
+
+plt.figure()
+
+display=DecisionBoundaryDisplay.from_estimator(best_model,X_test_pca,response_method='predict',response=y_pred_test,cmap=plt.cm.RdYlBu,alpha=0.3)
+display.plot()
+display.ax_.set_xlabel('PC1')
+display.ax_.set_ylabel('PC2')
+display.ax_.set_title('Decision Boundary Scatter TestData')
+print(Y_test)
+print(Y_encoded_test)
+display.ax_.scatter(
+    X_test_pca[:, 0],X_test_pca[:, 1], c=Y_encoded_test, edgecolor="black",label='Normal'
+)
+display.ax_.legend()
+
 plt.show()
